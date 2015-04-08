2d93cd4f7981432c814f6268dae60111f6fe9cdc
eabderh <eabderh@clemson.edu>
2015-04-07 23:22:01 -0400

    elias: worked on checkout
    checkout now displays all files in the argument and in the folders
    specified by the argument.

--- checkout.sh
+++ checkout.sh
@@ -2,16 +2,64 @@
 
 
 
-if [ $# -ne 1 ]; then
-	echo No file
+
+
+
+if [ $# -lt 1 ]; then
+	echo no file
 	exit 1
 fi
 
-if [ -d $1 ]; then
-	files="$(find $1 -maxdepth 1 -type f)"
-	echo $files
-fi
+echo full param
+echo $param
 
-if [ -f $1 ]; then
-	echo $1
-fi
+echo check files
+declare -a files
+for i in $@; do
+	echo $i
+	if [ -d $i ]; then
+		files=( ${files[@]} $(find $i -type f) )
+	else
+		files[${#files[@]} + 1]=$i
+	fi
+
+	echo files so far
+	echo ${files[@]}
+	echo ${#files[@]}
+done
+
+echo all files
+echo ${files[@]}
+echo ${#files[@]}
+declare -p files
+
+exit 0
+
+
+
+
+
+function copy {
+
+}
+
+function remove {
+
+}
+
+function update {
+
+}
+
+function check {
+
+}
+
+
+
+
+
+
+
+
+
--- vimsession.sh
+++ vimsession.sh
@@ -3,8 +3,8 @@
 vsfile="$(find . -maxdepth 1 -type f -iname ".*.vs.vim" | head -n 1)"
 
 if [ -z "$vsfile" ]; then
-	echo none found;
-	exit 0;
+	echo none found
+	exit 0
 fi
 
 #read -n 1 -s
