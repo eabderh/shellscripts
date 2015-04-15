baf8bf41a3e1fd64036754a1369779a4bbfe53dd
eabderh <eabderh@clemson.edu>
2015-04-14 23:18:15 -0400

    added comments to two files
    the sshchangekey.sh and vimsession.sh files now have descriptive
    comments.

--- sshchangekey.sh
+++ sshchangekey.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+# changes the ssh private key to one located in the ~/.ssh folder.
+# assumes that all key pairs, private and public, are stored in
+# subdirectories named with a descriptive and corresponding name.
 
-
+# check for arguments
 if [ $# -lt 1 ]; then
 	echo false usage
 	exit 1
 fi
 
-
+# check if ssh agent has been started
 if [ -z $SSH_AUTH_SOCK ]; then
 	echo agent not started
 	exit 1
 fi
 
+# delete all keys currently in use
 ssh-add -D
-ssh-add ~/.ssh/$1
+
+# add the target key
+ssh-add ~/.ssh/$1/id_rsa
 
 
 
--- vimsession.sh
+++ vimsession.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# finds and opens the vim session file in the current directory.
+
+# find all files within the current directory, excluding
+# subdirectories, with either .vs.vim or .vs as the extension. take
+# the first one found from the resulting list and remove the rest.
 vsfile="$(find . -maxdepth 1 \-type f \
 				-iname ".*.vs.vim" \
 				-o \
 	 			-iname ".*.vs" \
 				| head -n 1)"
 
+# check if a file was found
 if [ -z "$vsfile" ]; then
 	echo none found
 	exit 0
 fi
 
-#read -n 1 -s
+# open file as a vim session
 vim -S $vsfile
 
 
