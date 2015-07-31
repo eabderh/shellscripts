bf4a1ec5c0cc2e4cd5bb2e47fc75bbf805a97c37
eabderh <eabderh@clemson.edu>
2015-07-31 02:29:04 -0400

    added some stuff to template.sh
    didn't document it so have no clue where it came from. fuck.

--- templates/template.sh
+++ templates/template.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
 
+trap 'echo something went wrong' ERR
+
+# check for options in the argument
+while getopts ":d:" opt; do
+	case $opt in
+		s)
+			echo safe mode
+			safe_mode=true;
+			;;
+		d)
+			destination=$OPTARG
+			;;
+		\?)
+			echo invalid option
+			exit 0
+			;;
+	esac
+done
+
+shift $(($OPTIND - 1))
 
 
 
-
+# check if target path is valid
 if [ $# -lt 1 ]; then
 	echo no file
 	exit 1
 fi
+
+origin = $1
+base = ${origin##*/}
+
+ln -s origin base
 
 echo full param
 echo $param
